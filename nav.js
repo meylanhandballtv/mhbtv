43c3c7f701a91da80bfaeaacca5c27040785b2a4
meylanhandballtv <meylanhandballtv@gmail.com>
2026-06-25 00:43:49 +0200

    Update nav.js

--- nav.js
+++ nav.js
@@ -8,18 +8,12 @@
       <ul class="topbar-nav" id="mainNav">
         <li><a href="index.html" ${currentPage==='index.html'?'class="active"':''}>Accueil</a></li>
         <li><a href="accreditations.html" ${currentPage==='accreditations.html'?'class="active"':''}>Accréditations</a></li>
-        <li><a href="diffusions.html" ${currentPage==='diffusions.html'?'class="active"':''}>Diffusions</a></li>
+        <li><a href="composition.html" ${currentPage==='composition.html'?'class="active"':''}>Diffusions</a></li>
         <li><a href="equipe.html" ${currentPage==='equipe.html'?'class="active"':''}>L'Équipe</a></li>
         <li><a href="statuts.html" ${currentPage==='statuts.html'?'class="active"':''}>Statuts & Règlements</a></li>
-        <li class="nav-dropdown">
-          <a href="documentation.html" ${currentPage==='documentation.html'||currentPage==='audiences.html'||currentPage==='charte.html'?'class="active"':''}>Documentation</a>
-          <div class="dropdown-menu">
-            <a href="audiences.html">Audiences</a>
-            <a href="charte.html">Charte graphique</a>
-            <a href="documentation.html">Compte-rendus AG</a>
-          </div>
-        </li>
+        <li><a href="distinctions-reconnaissances.html" $ {currentPage==='distinctions-reconnaissances.html'?'class="active"':''}>Distinctions</a></li>
         <li><a href="gratification.html" ${currentPage==='gratification.html'?'class="active"':''}>Gratification</a></li>
+        <li><a href="documentation.html" ${currentPage==='documentation.html'?'class="active"':''}>Documentation</a></li>
         <li><a href="connexion.html">Se connecter</a></li>
       </ul>
       <div class="hamburger" id="hamburger" onclick="document.getElementById('mainNav').classList.toggle('open')">

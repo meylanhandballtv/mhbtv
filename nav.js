a7cbe0e4d27e5ba7873ce6c252d1de90c0f7b057
meylanhandballtv <meylanhandballtv@gmail.com>
2026-08-16 00:11:00 +0200

    Update nav.js

--- nav.js
+++ nav.js
@@ -12,7 +12,6 @@
         <li><a href="accreditations.html" ${currentPage==='accreditations.html'?'class="active"':''}>Accréditations</a></li>
         <li><a href="composition.html" ${currentPage==='composition.html'?'class="active"':''}>Diffusions</a></li>
         <li><a href="equipe.html" ${currentPage==='equipe.html'?'class="active"':''}>L'Équipe</a></li>
-        <li><a href="distinctions-reconnaissances.html" ${currentPage==='distinctions-reconnaissances.html'?'class="active"':''}>Distinctions</a></li>
         <li><a href="gratification.html" ${currentPage==='gratification.html'?'class="active"':''}>Gratification</a></li>
         <li><a href="documentation.html" ${currentPage==='documentation.html'?'class="active"':''}>Documentation</a></li>
         <li><a href="connexion.html">Se connecter</a></li>
